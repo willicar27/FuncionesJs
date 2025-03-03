cf68bc0ef9f2d5f74b92f3d45b79114d7220f7f8
willicar27 <willicar27@gmail.com>
2025-03-03 16:02:41 -0300

    completando el segundo ejercicio solicitado

---
+++ assets/JS/Script.js
@@ -0,0 +1,23 @@
+//esta es la forma original del enunciado del elejercicio 3
+/*function pintar(){
+    ele.style.backgroundColor = 'yellow'
+    }
+    const ele = document.getElementById("ele1")
+    ele.addEventListener("click", pintar); */
+
+    //respuesta del ejercicio 3
+
+    // const ele = document.getElementById("ele1")
+    // ele.addEventListener("click", function(){
+    //     ele.style.backgroundColor = yelow
+    // });
+
+//esta seria la respuesta de 2.3 del ejercicio
+
+function pintar(color = 'green') {
+    ele.style.backgroundColor = color;
+    }
+    const ele = document.getElementById("ele1")
+    ele.addEventListener("click", function() {
+        pintar();
+    });
